6a8b2e43a364f8e261effb659b0210da354d3b70
Alex474pr <zadanoleksandr91@gmail.com>
2024-06-19 07:38:28 +0300

    Add files via upload

---
+++ Курсач/Курсач/Курсач.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n, k;
+    cout << "Wedit kilkist misyatsiv: ";
+    cin >> n;
+    cout << "Wedit kilkist krolikiv kotrix zyidae monstr: ";
+    cin >> k;
+
+    int beginr = 1; 
+    int currentr = beginr; 
+
+    for (int i = 1; i <= n; ++i) {
+        int newr = currentr;
+        if (currentr > k) {
+            newr -= k;
+            currentr -= k;
+        }
+        beginr = currentr;
+        currentr += newr;
+    }
+    cout << "ostanniy misyats: " << currentr << " krolikiv" << endl;
+
+    return 0;
+}
